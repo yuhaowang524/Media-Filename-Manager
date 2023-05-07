d6a5e2d288ad44e873c7a2e3a05ea40bdbd0457d
Yuhao Wang <yuhaowang524@icloud.com>
2023-05-07 12:25:15 -0700

    MD file updated

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# media_filename_manager
-a small python script that rename media file according to dedicated naming rule adopted in Infuse player
+# Media Filename Manager
+
+## Abstract
+This small python script is designed to rename multiple media files (.mp4 or .mkv) into specific format
+that could easily be identified in Infuse media player.
+
+## Structure
+There are two versions of python scripts available. One dedicated to MacOS, the other supports WindowsOS.
+
+`main.py`
+
+run this python file in terminal in the following format
+<pre> $sudo main.py folder_address media_type name season_number/year episode mode </pre>
+* folder_address: the absolute address of mediafile
+* media_type: type `m` for movie, `t` for TV series
+* name: names you want to change your files into
+* season_number/year: enter season number for TV series or publish year for movie
+* episode: starting episode number
+* mode: `0` means a dry run, `1` means actual renaming. It is highly recommended that a dry run will be performed before the actual renaming process occurred
+
+## Reference && Citations
--- Windows/main.py
+++ Windows/main.py
@@ -3,11 +3,12 @@ import sys
 
 
 # This is a Python script designed to change file names according to
-# following naming rule <folder_address> <media_type> <name> <season_number/year> <mode>
+# following naming rule <folder_address> <media_type> <name> <season_number/year> <episode> <mode>
 # folder_address: the absolute path of media folder noted that media content must be within the folder
 # media_type: only 'm' or 't' are allowed 'm' means movie and 't' means tv shows
 # name: new name of media
 # season_number/year: if 't' is entered than season_number is followed else if year the movie is produced
+# episode: starting episode number
 # mode: 0 means a dry run 1 means actual rename it is highly recommended that a dry run will be performed
 # before the actual rename happens
 
@@ -22,23 +23,24 @@ def filename_changer(argv):
               "<season_number / year> <mode>")
 
     for file in folder_dir:
-        file_list.append(file)
+        file_ext = os.path.splitext(file)[-1].lower()
+        if file_ext == '.mp4' or file_ext == '.mkv':
+            file_list.append(file)
     file_list.sort()
 
     if argv[1] == 't':
-        if argv[4] == '0' or argv[4] == '1':
-            if argv[4] == '0':
+        if argv[5] == '0' or argv[5] == '1':
+            if argv[5] == '0':
                 print("This is the result of a dry run, please check before actual name changing")
 
-            for num, file in enumerate(file_list, 1):
+            for num, file in enumerate(file_list, int(argv[4])):
                 file_ext = os.path.splitext(file)[-1].lower()
-                if file_ext == '.mp4' or file_ext == '.mkv':
-                    old_path = file_address + "\\" + file
-                    new_name = argv[2] + '_S' + argv[3] + 'E' + str(num) + file_ext
-                    new_path = file_address + "\\" + new_name
-                    if argv[4] == '1':
-                        os.rename(old_path, new_path)
-                    print(file + " now changes to " + new_name)
+                old_path = file_address + "\\" + file
+                new_name = argv[2] + '_S' + argv[3] + 'E' + str(num) + file_ext
+                new_path = file_address + "\\" + new_name
+                if argv[5] == '1':
+                    os.rename(old_path, new_path)
+                print(file + " now changes to " + new_name)
 
         else:
             print("Error: invalid <mode> only allows '0' or '1' \nplease enter <folder_address> <type> <name> "
@@ -46,19 +48,18 @@ def filename_changer(argv):
             sys.exit(0)
 
     elif argv[1] == 'm':
-        if argv[4] == '0' or argv[4] == '1':
-            if argv[4] == '0':
+        if argv[5] == '0' or argv[5] == '1':
+            if argv[5] == '0':
                 print("This is the result of a dry run, please check before actual name changing")
 
             for file in file_list:
                 file_ext = os.path.splitext(file)[-1].lower()
-                if file_ext == '.mp4' or file_ext == '.mkv':
-                    old_path = file_address + "\\" + file
-                    new_name = argv[2] + '_' + argv[3] + file_ext
-                    new_path = file_address + "\\" + new_name
-                    if argv[4] == '1':
-                        os.rename(old_path, new_path)
-                    print(file + " now changes to " + new_name)
+                old_path = file_address + "\\" + file
+                new_name = argv[2] + '_' + argv[3] + file_ext
+                new_path = file_address + "\\" + new_name
+                if argv[5] == '1':
+                    os.rename(old_path, new_path)
+                print(file + " now changes to " + new_name)
 
         else:
             print("Error: invalid <mode> only allows '0' or '1' \nplease enter <folder_address> <type> <name> "
